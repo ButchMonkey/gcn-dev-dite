33f8cd1eb2ab0d6f1f88f3b9a1979584071f3178
ButchMonkey <patrice97@hotmail.co.uk>
2022-07-03 20:41:02 +0100

    Close sidebar menu on click

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { NavLink } from 'react-router-dom';
 import Icons from '../assets/Images/Icons';
@@ -7,12 +8,22 @@ import * as SidebarItems from '../assets/Categories';
 import './Sidebar.css';
 
 const Sidebar = () => {
+	const [isMenuOpen, handleMenu] = useState(false);
+	const handleCloseMenu = () => {
+		handleMenu(false);
+	};
+	const handleStateChange = (state) => {
+		handleMenu(state.isOpen);
+	};
+
 	return (
 		<Menu
 			customBurgerIcon={
 				<Icons.Menu style={{ height: 24, marginLeft: 8 }} />
 			}
 			customCrossIcon={false}
+			isOpen={isMenuOpen}
+			onStateChange={handleStateChange}
 		>
 			{Object.keys(SidebarItems).map((itemName) => {
 				if (!SidebarItems[itemName].link) {
@@ -25,6 +36,7 @@ const Sidebar = () => {
 						to={SidebarItems[itemName].link}
 						className="sidebar_menu_item_link"
 						activeClassName="sidebar_menu_item_link_active"
+						onClick={() => handleCloseMenu()}
 					>
 						<div className="sidebar_menu_item" key={itemName}>
 							{SidebarItems[itemName].name}
